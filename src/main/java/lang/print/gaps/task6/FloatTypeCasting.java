a5ee8e4db8ef799501b303869fbebe3b4ef3b51e
Aizhan-15 <aizhanurman@gmail.com>
2022-09-17 12:43:44 +0600

    completed all tasks

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        float first=19;
+        float first=19.6f;
         float firstRounded = Math.round(first);
         System.out.println(firstRounded);
     }
